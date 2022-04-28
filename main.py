43bee9ed842e2b4647342826a03034d6a31d3c62
daxlin13 <daxlin13@gmail.com>
2022-04-28 15:15:57 +0800

    fix time zone

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def update():
         else:
             now = datetime.datetime.now()
             print(now, "尚无更新")
-        if datetime.datetime.now().hour < 17:
+        if datetime.datetime.now().hour > 18:
             print('time up')
             exit()
         time.sleep(3600)  # 一小时检测一次
